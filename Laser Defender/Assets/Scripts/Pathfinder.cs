f878e2a19b688c85d9e6fc667ae0a37746d9630f
Mika Väisänen <vaisasenmika89@outlook.com>
2022-12-11 15:43:21 +0200

    Implemented functionality to player shooting

---
+++ Laser Defender/Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    private EnemySpawner enemySpawner;
+    private WaveConfigSO waveConfig;
+    private List<Transform> waypoints;
+    private int waypointIndex = 0;
+
+    private void Awake()
+    {
+        enemySpawner = FindObjectOfType<EnemySpawner>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveConfig = enemySpawner.GetCurrentWave();
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            Vector3 targetPosition = waypoints[waypointIndex].position;
+            float delta = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, delta);
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
